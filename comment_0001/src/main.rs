c7d05ddce3afea6d0c3f145015721669a3ebf53d
Nguyen Dai Son <daison.nguyen@rapsodo.com>
2022-11-19 09:39:38 +0800

    comment_0001 is to learn how to document a Rust crate.

---
+++ comment_0001/src/main.rs
@@ -0,0 +1,55 @@
+/// This program is to learn how to document the program.
+///
+/// Document comments:
+/// ```
+/// /// ...text...
+/// ```
+///
+/// Document comments:
+/// ```
+/// //! #...test..
+/// ```
+///
+/// Document comments:
+/// ```
+/// //! ...test...
+/// ```
+///
+/// Single-line comments:
+/// ```
+/// // ...text ...
+/// ```
+///
+/// Multiple-line comments:
+/// ```
+/// /*
+/// ..........
+/// ...line...
+/// ...line...
+/// ..........
+/// */
+/// ```
+///
+fn main() {
+    //! # `fn main()`
+    //! the `main` function, which is the entry point of the program.
+
+    // Hello, world!
+    println!("Hello, world!");
+
+    /*
+     * Some cargo commands:
+     *  cargo new comment_0001
+     *  cargo fmt
+     *  cargo check
+     *  cargo build
+     *  cargo test
+     *  cargo clippy
+     *  cargo run
+     *  cargo run --quiet
+     *  cargo run --bin comment_0001
+     *  cargo run --bin comment_0001 --quiet
+     *  cargo doc --open
+     *  cargo test
+     */
+}
