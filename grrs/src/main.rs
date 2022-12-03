0cba357d28b59bb7c790884c174088539de5e6e8
Nguyen Dai Son <daison.nguyen@rapsodo.com>
2022-12-03 11:17:50 +0800

    This program is to search for a pattern in a file and display the lines that contain it.

---
+++ grrs/src/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+fn main() {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
+
+/// Search for a pattern in a file and display the line that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
